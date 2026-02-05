18d50c411a5cc8abe0d8eddb49aab2baaf428a56
Dharmendra <145327275+Dharmendra0202@users.noreply.github.com>
2026-02-05 15:25:04 +0530

    removing theme from app folder

--- app/(tabs)/bookings.tsx
+++ app/(tabs)/bookings.tsx
@@ -12,7 +12,7 @@ import {
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
-import { colors, gradients } from "../theme/colors";
+import { colors, gradients } from "../../theme/colors";
 import { useRouter } from "expo-router";
 
 export default function BookingsScreen() {
