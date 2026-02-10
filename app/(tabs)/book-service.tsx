dc3f88b4ad8e8fce913d8170f1e514bff88c0846
Dharmendra <145327275+Dharmendra0202@users.noreply.github.com>
2026-02-10 20:12:08 +0530

    deleting the services part

--- app/(tabs)/book-service.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function BookServiceScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Book Service Screen</Text>
-    </View>
-  );
-}
